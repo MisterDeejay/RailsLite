16daf3576316b07474ce5a0e91c309c98660e1bd
Khaaliq DeJan <khaaliqdejan@gmail.com>
2015-03-27 15:08:17 -0700

    complete phase 4

--- lib/phase4/controller_base.rb
+++ lib/phase4/controller_base.rb
@@ -4,14 +4,22 @@ require_relative './session'
 module Phase4
   class ControllerBase < Phase3::ControllerBase
     def redirect_to(url)
+      super(url)
+      self.session.store_session(@res)
+      
+      nil
     end
 
     def render_content(content, content_type)
+      super(content, content_type)
+      self.session.store_session(@res)
+
+      nil
     end
 
     # method exposing a `Session` object
     def session
-      @session ||= Session.new(req)
+      @session ||= Session.new(@req)
     end
   end
 end
